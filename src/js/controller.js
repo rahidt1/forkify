cfb2a23c4952276924ec84d84f99513c0658d038
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2022-08-15 23:10:31 +0600

    Load recipe data from API.

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,28 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
+showRecipe();
